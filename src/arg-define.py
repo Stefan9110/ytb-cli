414538f5845538016e4481d651745f9c392ff206
stefan911 <danielstefan.halasz@gmail.com>
2022-02-08 16:59:14 +0200

    Added src

---
+++ src/arg-define.py
@@ -0,0 +1,6 @@
+import argparse
+import constants
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser(description=constants.PROJECT_DESCRIPTION)
---
+++ src/constants.py
@@ -0,0 +1,3 @@
+PROJECT_DESCRIPTION = "YouTube - Command Line. A minimal python script to manage" \
+                      " all your youtube media consumption within the terminal."
+
